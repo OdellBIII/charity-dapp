42eb0ad7c2c21b9a08f4956cb52605fa8d3bd72f
Odell Blackmon III <blackmoniii.odell@gmail.com>
2021-08-20 22:46:33 -0400

    Rewrote contract to be simpler. No testing has been completed

--- contracts/DownBad.sol
+++ contracts/DownBad.sol
@@ -3,53 +3,67 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract DownBad {
 
-	mapping(address => uint) public balances;
+	/*
+	mapping(address => uint) public downBadIndices;
 	mapping (address => bool) public downBadAddresses;
+	address payable downBadAddressesArr[];
+	*/
+
+	struct DownBadManager {
+		address payable[] downBadAddressesArr;
+		mapping (address => bool)  downBadAddressesPresent;
+		mapping (address => uint)  downBadIndices;
+	}
+
+	DownBadManager manager;
+
+	constructor() public {
+
+	}
 
 	function () external payable {
 
-		balances[msg.sender] += msg.value;
-	}
+		if(manager.downBadAddressesArr.length != 0){
+			uint numOfDownBad = manager.downBadAddressesArr.length;
+			uint split = msg.value / numOfDownBad;
 
-	function getBalance(address _addr) public view returns (uint) {
+			for(uint i = 0; i < numOfDownBad; i++){
 
-		return balances[_addr];
-	}
-
-	function getContractBalance() public view returns (uint) {
-
-		return address(this).balance;
-	}
-
-	function joinDownBad(address _downBad) public {
-
-		if(balances[msg.sender] > 0){
-
-			downBadAddresses[_downBad] = true;
+				manager.downBadAddressesArr[i].transfer(split);
+			}
 		}
 	}
 
 	function isDownBad(address _addr) public view returns (bool) {
 
-		return downBadAddresses[_addr];
+		return manager.downBadAddressesPresent[_addr];
+	}
+
+	function joinDownBad(address payable _downBad) public {
+
+		if(!isDownBad(_downBad)){
+
+			manager.downBadAddressesArr.push(_downBad);
+			manager.downBadAddressesPresent[_downBad] = true;
+			uint arrLength = manager.downBadAddressesArr.length;
+			manager.downBadIndices[_downBad] = arrLength - 1;
+
+		}
 	}
 
 
-	function withdraw() public {
 
-		if(downBadAddresses[msg.sender]){
+	function removeDownBad(address payable _downBad) public {
 
-			uint contractBalance = address(this).balance;
+		if(isDownBad(_downBad)){
 
-			msg.sender.transfer(contractBalance);
-			/*
-			(bool success, ) = msg.sender.call.value(contractBalance)("");
-			if(!success){
-				revert();
-			}
-			*/
-
+			uint index = manager.downBadIndices[_downBad];
+			uint arrLength = manager.downBadAddressesArr.length;
+			manager.downBadAddressesArr[index] = manager.downBadAddressesArr[arrLength - 1];
+			delete manager.downBadAddressesArr[arrLength - 1];
+			manager.downBadAddressesArr.length--;
 		}
+
 	}
 
 }
