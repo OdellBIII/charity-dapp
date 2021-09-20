a54061308eeb7f4ef86a63857ba98876b1dfa84e
Odell Blackmon III <blackmoniii.odell@gmail.com>
2021-09-20 15:04:52 -0400

    Working on adding truffle to node server

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ const port = 3000;
 
 const express = require('express');
 const app = new express();
+const contract = require('truffle-contract');
+const path = require('path');
+const Web3 = require('Web3');
+const DownBadJSON = require(path.join(__dirname, 'build/contracts/DownBad.json'));
+
+// Connect to network via RPC
+var provider = new Web.providers.HttpProvider("http://localhost:8545");
+
+
+// Read JSON and attach RPC connection
+var DownBad = contract(DownBadJSON);
+DownBad.setProvider(provider);
 
 app.get('/', function(req, res){
 
   res.sendFile('index.html', {root: __dirname});
+});
+
+app.put('/joinDownBad', function(req, res){
+
+  joinDownBad();
 });
 
 app.listen(port || 3000, () => {console.log("Server is up...")});
